f4b9bbe38eb3aacbfd07148d2f00d51196ff0a43
unknown <jnash51@live.com>
2022-12-11 03:28:13 +1000

    add new entities

--- ContosoUniversity/Data/SchoolContext.cs
+++ ContosoUniversity/Data/SchoolContext.cs
@@ -11,12 +11,24 @@ namespace ContosoUniversity.Data
         public DbSet<Course> Courses { get; set; }
         public DbSet<Enrollment> Enrollments { get; set; }
         public DbSet<Student> Students { get; set; }
+        public DbSet<Department> Departments { get; set; }
+        public DbSet<Instructor> Instructors { get; set; }
+        public DbSet<OfficeAssignment> OfficeAssignments { get; set; }
+        public DbSet<CourseAssignment> CourseAssignments { get; set; }
+
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<Course>().ToTable("tblCourse");
             modelBuilder.Entity<Enrollment>().ToTable("tblEnrollment");
             modelBuilder.Entity<Student>().ToTable("tblStudent");
+            modelBuilder.Entity<Department>().ToTable("tblDepartment");
+            modelBuilder.Entity<Instructor>().ToTable("tblInstructor");
+            modelBuilder.Entity<OfficeAssignment>().ToTable("tblOfficeAssignment");
+            modelBuilder.Entity<CourseAssignment>().ToTable("tblCourseAssignment");
+
+            modelBuilder.Entity<CourseAssignment>()
+                .HasKey(c => new { c.CourseID, c.InstructorID });
         }
     }
 }
